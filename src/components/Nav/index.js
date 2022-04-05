f6382371b64aba1f0f6597218fd38d969e834907
jacob nordan <jacob.nordan113322@gmail.com>
2022-04-05 16:29:08 -0700

    added basic bootstrap hamburger menu for navbar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,12 +1,50 @@
-
 function Nav() {
-    return (
-        <div className="nav">
-            <div className="navLinks">
-                
-            </div>
+  return (
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container-fluid">
+          <h4>Jacob Nordan</h4>
+          <button
+            class="navbar-toggler collapsed d-flex d-lg-none flex-column justify-content-around"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="toggler-icon top-bar"></span>
+            <span class="toggler-icon middle-bar"></span>
+            <span class="toggler-icon bottom-bar"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#about">
+                  About
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#skills">
+                  Skills
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#experience">
+                  Experience
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#portfolio">
+                  Portfolio
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-    )
+      </nav>
+    </div>
+  );
 }
 
 export default Nav;
