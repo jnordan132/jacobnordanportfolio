612e40796c58c59a3065ce14d00aa619fb31b44b
jacob nordan <jacob.nordan113322@gmail.com>
2022-04-05 17:06:42 -0700

    added profile picture

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Nav from './components/Nav';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <br />
+      <About />
     </div>
   );
 }
---
+++ src/components/About/index.js
@@ -0,0 +1,17 @@
+import pfp from "../../assets/JacobNordan-2.jpg";
+
+function About() {
+
+    return (
+        <div className="about">
+            <div className="profilePic">
+                <img src={pfp}></img>
+            </div>
+            <div className="aboutMe">
+
+            </div>
+        </div>
+    )
+};
+
+export default About;
