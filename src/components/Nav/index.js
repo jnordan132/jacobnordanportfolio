3e31e4f4440c0d527a0ba4b4ebe2162bfdc6d04f
jacob nordan <jacob.nordan113322@gmail.com>
2022-04-20 17:54:31 -0400

    test purpose

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -25,7 +25,7 @@ function Nav() {
                 <li class="nav-item">
                   <Link class="category" to="about" offset={-50}>
                     <a href="about" class="nav-link active">
-                      About
+                      About test
                     </a>
                   </Link>
                 </li>
